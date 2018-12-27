57b671292357ec99178f4fcf369770030e96c136
Matthew Mangan <M27Mangan@users.noreply.github.com>
2018-12-27 14:27:45 -0500

    Mobile Nav
    Fixes for Bulma's mobile nav classes and JS toggles

--- js/all.js
+++ js/all.js
@@ -9,7 +9,14 @@
      $('.page-loader div').delay(0).fadeOut()
      $('.page-loader').delay(200).fadeOut('slow')
    })
- 
+   $(document).ready(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+    });
+  });
    $(document).ready(function () {
      $('.newLogo').click(function () {
        $('.fall-off').addClass('hinge')
